5db992b7fe1ab2fe64ec56a2fd8ea62a2987a6aa
KelvinoKing <kelvinoking@outlook.com>
2023-06-25 07:51:40 +0000

    updates

--- print-string.c
+++ print-string.c
@@ -6,20 +6,12 @@
  */
 int _print_string(va_list ap)
 {
-	int i = 0;
-	char c;
 	char *str = va_arg(ap, char*);
 	int lenstr = 0;
 
 	if (str == NULL)
 		str = "(null)";
 	lenstr = _strlen(str);
-	while (i < lenstr)
-	{
-		c = str[i];
 
-		write(1, &c, 1);
-		i++;
-	}
-	return (lenstr);
+	return (write(1, str, lenstr));
 }
