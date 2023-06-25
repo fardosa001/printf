07ab14f58ac7b97ca7e76fe1f82305b321d3810a
KelvinoKing <kelvinoking@outlook.com>
2023-06-25 06:40:29 +0000

    Contains all prototypes of functions

--- main.h
+++ main.h
@@ -1,8 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+/**
+ * struct my_struct - Struct to hold special chars and function pointers
+ * @c: hold special characters
+ * @f: function pointers
+ *
+ * Description: My struct to help me
+ */
+typedef struct my_struct
+{
+	char c;
+	int (*f)(va_list);
+} op_t;
 
 int _printf(const char *format, ...);
 int _print_char(va_list ap);
 int _print_string(va_list ap);
-
+int _strlen(char *str);
 #endif
